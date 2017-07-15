c7a4a1595a476f519415fe577394dee9b61bef93
tylercwilliams <tylerwilliams.c@gmail.com>
2017-07-14 22:52:05 -0400

    clean up code

--- model/index.js
+++ model/index.js
@@ -1,31 +1,31 @@
 let sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname');
 
 let User = sequelize.define('user', {
-	username: {
-		type: Sequelize.STRING;
-	},
-	slackId: {
-		type: Sequelize.STRING;
-	}
+  username: {
+    type: Sequelize.STRING,
+  },
+  slackId: {
+    type: Sequelize.STRING,
+  },
 });
 
 let Channel = sequelize.define('channel', {
-	channelName: {
-		type: Sequelize.STRING;
-	},
-	channelId: {
-		type: Sequelize.STRING;
-	}
+  channelName: {
+    type: Sequelize.STRING,
+  },
+  channelId: {
+    type: Sequelize.STRING,
+  },
 });
 
 let Project = sequelize.define('project', {
-	projectName: {
-		type: Sequelize.STRING;
-	},
-	projectRepoUrl: {
-		type: Sequelize.STRING;
-	}
-}
+  projectName: {
+    type: Sequelize.STRING,
+  },
+  projectRepoUrl: {
+    type: Sequelize.STRING,
+  },
+});
 
 Project.hasMany(User, { as: 'Contributors' });
 User.hasMany(Project, { as: 'Projects' });
--- routes/callback.js
+++ routes/callback.js
@@ -2,49 +2,49 @@ let express = require('express');
 let router = express.Router();
 
 router.get('/', (req, res, next) => {
-	
+
 
 });
 
 router.post('/', (req, res, next) => {
-	let text = res.body.text;
-	let tokens = text.split(" ");
-	
-	let username = tokens[0];
-	let command = tokens[1];
-	let channelName = tokens[3];
-	
-	let userRe = /<@[a-zA-Z0-9]+\|[0-9a-zA-Z_-]+>/g;
-	let channelRe = /<@[a-zA-Z0-9]+\|[0-9a-zA-Z_-]+>/g;
+  let text = res.body.text;
+  let tokens = text.split(' ');
 
-	// error state for malformed command.
-	if (!username.matches(escapedRe)) {
+  let username = tokens[0];
+  let command = tokens[1];
+  let channelName = tokens[3];
 
-	};
+  let userRe = /<@[a-zA-Z0-9]+\|[0-9a-zA-Z_-]+>/g;
+  let channelRe = /<#[a-zA-Z0-9]+\|[0-9a-zA-Z_-]+>/g;
 
-	if (!(channelName != "add") || (channelName != "remove")) {
+  // Error state for malformed command.
+  if (!username.matches(escapedRe)) {
 
-	};
+  }
 
-	if (!channelName.matches(escapedRe)) {
+  if ((channelName != 'add') || (channelName != 'remove')) {
 
-	};
+  }
 
-	if (command == 'add' && type == 'user') {
-		res.status(200).send({
-			response_type: 'ephemeral',
-				text: 'add user'
-		});
-		addUser();
-	}
+  if (!channelName.matches(escapedRe)) {
 
-	if (command == 'add' && type == 'project') {
-		res.status(200).send({
-			response_type: 'ephemeral',
-				text: 'add user'
-		});
-		addProject();
-	}
+  }
+
+  if (command == 'add' && type == 'user') {
+    res.status(200).send({
+      response_type: 'ephemeral',
+      text: 'add user',
+    });
+    addUser();
+  }
+
+  if (command == 'add' && type == 'project') {
+    res.status(200).send({
+      response_type: 'ephemeral',
+      text: 'add user',
+    });
+    addProject();
+  }
 
 });
 
@@ -53,12 +53,15 @@ function addUser() {
 }
 
 function addProject() {
+
 }
 
 function removeUser() {
+
 }
 
 function removeProject() {
+
 }
 
 module.exports = router;
